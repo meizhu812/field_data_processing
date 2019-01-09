463e60fbc61b84c2d84e81c9a15fb86c1fff3acb
Zhu Mei <meizhu812@outlook.com>
2019-01-10 00:01:47 +0800

    dataclass

--- core.py
+++ core.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from itertools import islice
 from pandas.tseries.offsets import Minute
 from pandas import concat, DataFrame, read_csv, read_pickle
@@ -7,71 +8,75 @@ import os
 from _datatools import get_files_list
 
 
+@dataclass
 class ProjectConfig:
-    def __init__(self, name: str, path: str, raw_sub: str, output_sub: str, freq: str, cpus: int):
-        self.NAME = name
-        self.PATH = path
-        self.RAW_SUB = raw_sub
-        self.OUTPUT_SUB = output_sub
-        self.TEMP_PATH = self.PATH + r'\temp'
-        self.FREQ = freq
-        self.CPUS = cpus
+    name: str
+    path: str
+    raw_sub: str
+    output_sub: str
+    freq: str
+    cpus: int
+
+    def __post_init__(self):
+        self.temp_path = self.path + r'\temp'
 
     def __enter__(self):
-        print("\nProcessing data from project[{}]".format(self.NAME))
+        print("\nProcessing data from project[{}]".format(self.name))
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print("Finished processing project[{}]".format(self.NAME))
+        print("Finished processing project[{}]".format(self.name))
 
 
+@dataclass
 class DataSet:
-    def __init__(self, project: ProjectConfig, name: str, data_type: str, sub_path: str, data_format: dict, file_pattern,
-                 data_period=None):
-        self.PROJECT = project
-        self.NAME = name
-        self.TYPE = data_type
-        self.RAW_PATH = project.PATH + project.RAW_SUB + sub_path
-        self.TEMP_PATH = project.TEMP_PATH + sub_path + '\\'
-        self.OUTPUT_PATH = project.PATH + project.OUTPUT_SUB + sub_path + '\\'
-        self.DATA_FORMAT = data_format
-        self.FILE_PATTERN = file_pattern
-        self.DATA_PERIOD = data_period
-        self.data_files = []
-        self.data = DataFrame()
+    project: ProjectConfig
+    name: str
+    data_type: str
+    sub_path: str
+    data_format: dict
+    file_pattern: dict
+    data_period: None
+    data_files = []
+    data = DataFrame()
+
+    def __post_init__(self):
+        self.raw_path = self.project.path + self.project.raw_sub + self.sub_path
+        self.temp_path = self.project.temp_path + self.sub_path + '\\'
+        self.output_path = self.project.path + self.project.output_sub + self.sub_path + '\\'
 
     def __enter__(self):
-        print("\nProcessing data set [{}]".format(self.NAME))
+        print("\nProcessing data set [{}]".format(self.name))
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print("Finished processing data set [{}]".format(self.NAME))
+        print("Finished processing data set [{}]".format(self.name))
 
     def get_data(self):
         try:
             self.data = self._read_pickle()
         except FileNotFoundError:
             print("Pickle file does not exist!")
-            self.data_files = get_files_list(path=self.RAW_PATH, **self.FILE_PATTERN)
+            self.data_files = get_files_list(path=self.raw_path, **self.file_pattern)
             self._merge_data()
 
     def process_data(self):
-        if self.TYPE == 'SONIC':
+        if self.data_type == 'SONIC':
             self._sonic_split()
-        elif self.TYPE == 'AMMONIA':
+        elif self.data_type == 'AMMONIA':
             self._ammonia_resample()
         else:
             print("Not processed.")
 
     def _read_pickle(self):
-        pickle_path = self.TEMP_PATH + self.NAME
+        pickle_path = self.temp_path + self.name
         print("# Reading data from pickle:\n"
               "# [{}]".format(pickle_path))
         data = read_pickle(pickle_path)
         return data
 
     def _save_data(self):
-        pickle_path = self.TEMP_PATH + self.NAME
+        pickle_path = self.temp_path + self.name
         print("# Saving data to pickle:\n"
               "# [{}]".format(pickle_path))
         self.data.to_pickle(pickle_path)
@@ -85,30 +90,30 @@ class DataSet:
     def _merge_data(self):
         timer_merge = Timer()
         timer_merge.start("Reading data files for merging", "Initializing")
-        with Pool(self.PROJECT.CPUS) as p:
-            timer_merge.switch("Reading with %i processes" % self.PROJECT.CPUS)
+        with Pool(self.project.cpus) as p:
+            timer_merge.switch("Reading with %i processes" % self.project.cpus)
             datum_async_list = p.starmap_async(self._read_data_file,
-                                               [(data_file, self.DATA_FORMAT) for data_file in self.data_files])
+                                               [(data_file, self.data_format) for data_file in self.data_files])
             show_progress(datum_async_list)
             datum_list = datum_async_list.get()
         timer_merge.switch("Merging data")
         self.data = concat(datum_list)
         timer_merge.switch("Saving data")
-        os.makedirs(self.TEMP_PATH, exist_ok=True)
+        os.makedirs(self.temp_path, exist_ok=True)
         self._save_data()
         timer_merge.stop()
 
     def _sonic_split(self):
         data_quality = DataFrame([])
-        data_quality['time'] = self.DATA_PERIOD
+        data_quality['time'] = self.data_period
         data_quality['count'] = -1
-        os.makedirs(self.OUTPUT_PATH, exist_ok=True)
+        os.makedirs(self.output_path, exist_ok=True)
         timer_split = Timer()
         timer_split.start("Splitting data files", "Processing data")
-        data_period_divided_all = self.data_period_divide(self.data, self.DATA_PERIOD, self.PROJECT.CPUS)
-        with Pool(self.PROJECT.CPUS) as p:
+        data_period_divided_all = self.data_period_divide(self.data, self.data_period, self.project.cpus)
+        with Pool(self.project.cpus) as p:
             timer_split.switch('Writing data files')
-            results = [p.apply_async(self.split_data, (data_period_divided[0], data_period_divided[1], self.OUTPUT_PATH)) for
+            results = [p.apply_async(self.split_data, (data_period_divided[0], data_period_divided[1], self.output_path)) for
                        data_period_divided in data_period_divided_all]
             print(len(results))
             for result in results:
@@ -152,6 +157,6 @@ class DataSet:
         print("\nResampling data")
         self.data = self.data.tshift(8, freq='H')
         self.data = self.data.tshift(15, freq='T')
-        data_resamp = self.data.resample(self.PROJECT.FREQ).mean()
-        os.makedirs(self.OUTPUT_PATH, exist_ok=True)
-        data_resamp.to_csv(self.OUTPUT_PATH + r'\data_resamp.csv')
+        data_resamp = self.data.resample(self.project.freq).mean()
+        os.makedirs(self.output_path, exist_ok=True)
+        data_resamp.to_csv(self.output_path + r'\data_resamp.csv')
